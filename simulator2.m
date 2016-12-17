309628143cfc331e722250f0eb6395cb384fbd7a
Isacari <luisetron92@gmail.com>
2016-12-17 18:28:30 +0100

    All images of the article
    I already coded all the images in the article, are 10 in total, now we
    have to debug the errors and try to fit them to the correct values.
    Notice that in some cases (when Rd, Rs are not cero) the values can be a
    little displaced and it's correct.

--- simulator2.m
+++ simulator2.m
@@ -86,7 +86,7 @@ clear;
    
     
     %Fig. 7  Frégonèse et al. 2013%
-    %Redefine useful parameters
+    %Redefine useful parameters, Table I
     Nf = 2.3e16; %m^-2 
     Wox = 5e-6; %m
     u = 0.6570; %[m^2/Vs] 
@@ -120,3 +120,164 @@ clear;
         ylabel('Ids [uA]');
        
     end
+    %end Fig. 7  Frégonèse et al. 2013%
+    
+    %Fig. 8  Frégonèse et al. 2013%
+    %Redefine useful parameters Table II
+    Nf = 1.26e16; %m^-2 
+    Wox = 4e-6; %m
+    u = 0.6800; %[m^2/Vs] 
+    Er = 3.9; %No unit
+    Tox = 28e-9; %m 
+    Lox = 2.94e-6; %m  
+    w = 370e-3*1.602e-19/(planckConstantEVs/(2*pi)); %frecuency 1/s
+    spatialHom = 65e-3*1.602e-19; %J 
+    Npuddle = ((spatialHom)^2)/((((planckConstantEVs/(2*pi))*fermiVelocity)^2)*pi); %1/m^2
+    Ctop = calculateCtop(Er, Tox); %F/m^2
+    
+    
+    simSize = 100;
+    for Vgate = 0:0.5:3
+        Vgs = Vgate;
+        Vds = linspace(0,1,simSize); %We checked for both signs already and
+        %We indent to show that here starts the simulation 
+       
+          s = sign(Ctop*(Vgs-Vds/2) + electronCharge*Nf);
+          Qav = calculateQav(beta, Ctop, Vgs, Vds, electronCharge, Nf, s);
+          denominator = calculateDenominatorId(Vds, u, Qav, electronCharge, Npuddle, Lox, w); %this is just the denominator in ec. (5)
+          numerator = calculateNumeratorId(electronCharge, u, Wox, Ctop, beta, Npuddle, Nf, Vgs, Vds); %This is the intregral in the numerator of ec 5 
+          Id = (calculateId(numerator, denominator));
+      
+        %Now we start the plot
+        figure (5);
+        hold on;
+        Id = Id*10^6/(Lox*10^6); %We can also divide by Wox instead of Lox, to get the correct units and correct values
+        plot(Vds, Id,'.');
+        xlabel('Vdsi [V]');
+        ylabel('Ids [uA/um]');
+       
+    end
+    %end Fig. 8  Frégonèse et al. 2013%
+    
+    
+    %Fig. 9, 9.1  Frégonèse et al. 2013%
+    %Redefine useful parameters Table II
+    Nf = 2.5e15; %m^-2 
+
+    
+    simSize = 100;
+    Vgs = linspace(0,3,simSize);
+    
+    for Vdrain_source = [1.05, 1.55, 2.05, 2.55, 3.05]
+        Vds = Vdrain_source;
+        %Now we ident the simulation of Ids as a function of Vgs
+          
+            s = sign(Ctop*(Vgs-Vds/2) + electronCharge*Nf);
+            Qav = calculateQav(beta, Ctop, Vgs, Vds, electronCharge, Nf, s);
+            denominator = calculateDenominatorId(Vds, u, Qav, electronCharge, Npuddle, Lox, w); %this is just the denominator in ec. (5)
+            numerator = calculateNumeratorId(electronCharge, u, Wox, Ctop, beta, Npuddle, Nf, Vgs, Vds); %This is the intregral in the numerator of ec 5 
+            Id = (calculateId(numerator, denominator));
+              
+        %Now we plot the results
+        figure(6);
+        hold on;
+        Ids = Id*10^6/(Lox*10^6); %We could divide per Wox if that fits the values, since the author did not specify it
+        plot(Vgs, Ids, '.','markersize',5); 
+        xlabel('Vgs [V]');
+        ylabel('Ids [uA/um]');
+        
+        gm = Ids./Vgs; %Transconductance, we are not sure if this is the formula
+        figure(7);
+        hold on;
+        plot(Vgs, gm, '.','markersize',5); 
+        xlabel('Vgs [V]');
+        ylabel('gm [mS/mm]');
+        
+    end %end Fig. 9, 9.1  Frégonèse et al. 2013%
+    
+    %Fig. 10.1, 10.2, 10.3  Frégonèse et al. 2013%
+    %Redefine useful parameters Table II
+    Nf = 0; %m^-2 
+    Wox = 1; %The value cancels out and we report it per unit of length
+    u = 0.7000; %[m^2/Vs] 
+    Er = 3.5; %No unit
+    Tox = 8.5e-9; %m 
+    Lox = 440e-9; %m  
+    w = 56e-3*1.602e-19/(planckConstantEVs/(2*pi)); %frecuency 1/s
+    spatialHom = 66.8e-3*1.602e-19; %J 
+    Npuddle = ((spatialHom)^2)/((((planckConstantEVs/(2*pi))*fermiVelocity)^2)*pi); %1/m^2
+    Ctop = calculateCtop(Er, Tox); %F/m^2
+    
+    %Fig. 10.1 
+    simSize = 100;
+    for Vgate = 0:0.25:2
+        Vgs = Vgate;
+        Vds = linspace(0,1.5,simSize); %We checked for both signs already and
+        %We indent to show that here starts the simulation 
+       
+          s = sign(Ctop*(Vgs-Vds/2) + electronCharge*Nf);
+          Qav = calculateQav(beta, Ctop, Vgs, Vds, electronCharge, Nf, s);
+          denominator = calculateDenominatorId(Vds, u, Qav, electronCharge, Npuddle, Lox, w); %this is just the denominator in ec. (5)
+          numerator = calculateNumeratorId(electronCharge, u, Wox, Ctop, beta, Npuddle, Nf, Vgs, Vds); %This is the intregral in the numerator of ec 5 
+          Id = (calculateId(numerator, denominator));
+      
+        %Now we start the plot
+        figure (8);
+        hold on;
+        Id = Id*10^3/(10^6); 
+        plot(Vds, Id,'.');
+        xlabel('Vdsi [V]');
+        ylabel('Ids [mA/um]');
+        
+    end
+    %end Fig. 10.1%
+    
+    %Fig. 10.2
+    Lox = 1e-6; %m
+    simSize = 100;
+    for Vgate = 0:0.25:2
+        Vgs = Vgate;
+        Vds = linspace(0,1.5,simSize); %We checked for both signs already and
+        %We indent to show that here starts the simulation 
+       
+          s = sign(Ctop*(Vgs-Vds/2) + electronCharge*Nf);
+          Qav = calculateQav(beta, Ctop, Vgs, Vds, electronCharge, Nf, s);
+          denominator = calculateDenominatorId(Vds, u, Qav, electronCharge, Npuddle, Lox, w); %this is just the denominator in ec. (5)
+          numerator = calculateNumeratorId(electronCharge, u, Wox, Ctop, beta, Npuddle, Nf, Vgs, Vds); %This is the intregral in the numerator of ec 5 
+          Id = (calculateId(numerator, denominator));
+      
+        %Now we start the plot
+        figure (9);
+        hold on;
+        Id = Id*10^3/(10^6); 
+        plot(Vds, Id,'.');
+        xlabel('Vdsi [V]');
+        ylabel('Ids [mA/um]');
+        
+    end
+    %end Fig. 10.2%
+    
+    %Fig. 10.3
+    Lox = 3e-6; %m
+    simSize = 100;
+    for Vgate = 0:0.25:2
+        Vgs = Vgate;
+        Vds = linspace(0,1.5,simSize); %We checked for both signs already and
+        %We indent to show that here starts the simulation 
+       
+          s = sign(Ctop*(Vgs-Vds/2) + electronCharge*Nf);
+          Qav = calculateQav(beta, Ctop, Vgs, Vds, electronCharge, Nf, s);
+          denominator = calculateDenominatorId(Vds, u, Qav, electronCharge, Npuddle, Lox, w); %this is just the denominator in ec. (5)
+          numerator = calculateNumeratorId(electronCharge, u, Wox, Ctop, beta, Npuddle, Nf, Vgs, Vds); %This is the intregral in the numerator of ec 5 
+          Id = (calculateId(numerator, denominator));
+      
+        %Now we start the plot
+        figure (10);
+        hold on;
+        Id = Id*10^3/(10^6); 
+        plot(Vds, Id,'.');
+        xlabel('Vdsi [V]');
+        ylabel('Ids [mA/um]');
+        
+    end
+    %end Fig. 10.3%
